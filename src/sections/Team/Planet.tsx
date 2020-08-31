a8dc84a29d3108c7839f2ae814a81e71d6624cb5
Dmitry Dergunov <dergunovdv@yandex-team.ru>
2020-08-31 06:03:41 +0300

    960px+

--- src/sections/Team/Planet.tsx
+++ src/sections/Team/Planet.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 import { Parallax } from "react-scroll-parallax/cjs";
 
 import tShirt from "./assets/t-shirt.png";
@@ -12,22 +13,22 @@ import css from "./Planet.module.sass";
 
 export const Planet: React.FC = () => (
   <div className={css.wrap}>
-    <Parallax className={css.tShirt} y={[-10, 10]}>
+    <Parallax className={classNames(css.tShirt, css.image)} y={[-10, 10]}>
       <img src={tShirt} alt="" />
     </Parallax>
-    <Parallax className={css.rocket} y={[-10, 10]}>
+    <Parallax className={classNames(css.rocket, css.image)} y={[10, -10]}>
       <img src={rocket} alt="" />
     </Parallax>
-    <Parallax className={css.stateBoard} y={[-15, 15]}>
+    <Parallax className={classNames(css.stateBoard, css.image)} y={[15, -15]}>
       <img src={stateBoard} alt="" />
     </Parallax>
-    <Parallax className={css.mask} y={[-20, 20]}>
+    <Parallax className={classNames(css.mask, css.image)} y={[-20, 20]}>
       <img src={mask} alt="" />
     </Parallax>
-    <Parallax className={css.redbull} y={[-35, 35]}>
+    <Parallax className={classNames(css.redbull, css.image)} y={[35, -35]}>
       <img src={redbull} alt="" />
     </Parallax>
-    <Parallax className={css.meteor} y={[-30, 30]}>
+    <Parallax className={classNames(css.meteor, css.image)} y={[-30, 30]}>
       <img src={meteor} alt="" />
     </Parallax>
   </div>
